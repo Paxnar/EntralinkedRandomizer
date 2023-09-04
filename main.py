74e8255a1495e03eb57287f8588bd0c9b7174ef9
PavelUkilevich <ukilevich@yandex.eu>
2023-09-04 22:07:15 +0300

    Random

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ pkm_count = int(input())
 gsid = input()
 location = random.choice(locations.locations)
 
-
 g = requests.Session()
-encounters = [{'species': pokedata.pkms[random.choice(location)['name']]['id'], 'move': 75, 'form': '0', 'gender': 'GENDERLESS', 'animation': 'LOOK_AROUND'} for i in range(pkm_count)]
+encounters = []
+for i in range(pkm_count):
+    pkm = random.choice(location)
+    pkm_d = pokedata.pkms[pkm['name']]
+    pkm_id = pkm_d['id']
+    move = pokedata.moves[random.choice(pkm['move'])]
+    form = random.choice(pkm_d['forms']) if pkm['name'] \
+                                           not in ['Deoxys', 'Rotom', 'Giratina', 'Shaymin', 'Arceus', 'Tornadus',
+                                                   'Thundurus', 'Landorus', 'Kyurem', 'Keldeo',
+                                                   'Meloetta', 'Genesect'] else 0
+    animation = random.choice(["LOOK_AROUND", "WALK_AROUND", "WALK_LOOK_AROUND", "WALK_VERTICALLY",
+                               "WALK_HORIZONTALLY", "WALK_LOOK_HORIZONTALLY", "SPIN_RIGHT", "SPIN_LEFT"])
+    encounters.append(
+        {'species': pkm_id, 'move': move, 'form': form, 'gender': pkm_d['gender'], 'animation': animation})
 print(g.post('http://127.0.0.1/dashboard/login', data={
     'gsid': gsid}))
 print(g.post('http://127.0.0.1/dashboard/profile', data=json.dumps({'encounters': encounters,
