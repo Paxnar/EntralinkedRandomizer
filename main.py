3dea4327caa733e7a8fc0276ac6b8aa6259aff64
PavelUkilevich <ukilevich@yandex.eu>
2023-09-04 18:02:01 +0300

    Commit Message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 g = requests.Session()
 encounters = [{'species': random.randint(1, 649), 'move': 75, 'form': '0', 'gender': 'GENDERLESS', 'animation': 'LOOK_AROUND'} for i in range(random.randint(1, 10))]
 print(g.post('http://127.0.0.1/dashboard/login', data={
-    'gsid': 'LGKK8TS6YH'}))
+    'gsid': input()}))
 print(g.post('http://127.0.0.1/dashboard/profile', data=json.dumps({'encounters': encounters,
                                                                     'items': [],
                                                                     'avenueVisitors': [],
